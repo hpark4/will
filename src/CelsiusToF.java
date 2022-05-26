8e2c5967f1793bf2aa541b712a1942061d6ae30a
hpark4 <hpark4@gmail.com>
2022-05-25 21:57:48 -0400

    first two questions

---
+++ src/CelsiusToF.java
@@ -0,0 +1,26 @@
+/**
+ * class name:
+ * author:
+ * date:
+ * version:
+ * description:
+ * Write a program that converts a temperature from Celsius to Fahrenheit.
+ * It should (1) prompt the user for input, (2) read a double value from the keyboard,
+ * (3) calculate the result, and (4) format the output to one decimal place using printf.
+ * For example, it should display "24.0 C = 75.2 F".
+
+ * Here is the formula. Be careful not to use integer division! F = 9/5 C + 32
+ *
+ * Sample runs:
+ * Enter a temperature in Celsius: 24
+ * 24.0 C = 75.2 F
+ *
+ * Enter a temperature in Celsius: 100.078
+ * 100.1 C = 212.1 F
+ *
+ * Enter a temperature in Celsius: 12.345
+ * 12.3 C = 54.2 F
+ */
+public class CelsiusToF{
+
+}
---
+++ src/Fruits.java
@@ -0,0 +1,16 @@
+/**
+ * class name:
+ * author:
+ * date:
+ * version:
+ * description:
+ * Write a program named Banana.java that displays as below:
+ * A banana is an edible fruit.
+ *
+ * If you wait the correct amount of time for it to ripen,
+ *
+ * it will be sweet and delicious.
+ */
+public class Fruits {
+
+}
